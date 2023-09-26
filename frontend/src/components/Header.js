0d80b9d1a18542c83fa337ea62be912efd0f5dff
dymasius12 <dymasius12@gmail.com>
2023-09-26 15:58:57 +0800

    Added React-Bootstrap Header made on Tue Sep 26 15:58:57 +08 2023

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 
 function Header() {
   return (
     <div>
-        <header>Header</header>
+      <header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
