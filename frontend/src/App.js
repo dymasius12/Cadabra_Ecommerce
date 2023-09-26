b8affcae6389f507128cb3f817bff3a3ff85e6ed
dymasius12 <dymasius12@gmail.com>
2023-09-26 15:22:36 +0800

    Finished making placeholder for Header and Footer made on Tue Sep 26 15:22:36 +08 2023

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-
+import Header from './components/Header.js'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
+      <Header />
       Cadabra My App
+      <Footer />
     </div>
   );
 }
