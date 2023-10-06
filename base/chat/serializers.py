1a9476a3976a6b7f9084cdd4b429cff920ba1ddb
chenzi <chenzi8888@hotmail.com>
2023-10-07 03:27:18 +0800

    Fixed imports

--- base/chat/serializers.py
+++ base/chat/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Chat, Message
+from ..models import Chat, Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
--- base/chat/views.py
+++ base/chat/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Chat, Message
+from ..models import Chat, Message
 from .serializers import ChatSerializer, MessageSerializer
 
 
